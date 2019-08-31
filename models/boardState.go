a999cfefdc247d783a3fba74aaec0beb45710a52
atoyr <lessironglance@gmail.com>
2019-09-01 08:18:54 +0900

    update boardState tags add omitempty

--- models/boardState.go
+++ models/boardState.go
@@ -4,6 +4,6 @@ type BoardState struct {
 	Health string `json:"health"`
 	State  string `json:"state"`
 	Data   struct {
-		SpecialQuotation int `json:"special_quotation"`
-	} `json:"data"`
+		SpecialQuotation int `json:"special_quotation,omitempty"`
+	} `json:"data,omitempty"`
 }
