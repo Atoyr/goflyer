67afb73d6b38499ffba2b63b50e6634648732bb4
atoyr <lessironglance@gmail.com>
2019-09-01 21:51:31 +0900

    add board merge

--- models/board.go
+++ models/board.go
@@ -15,5 +15,28 @@ func (b *Board) Merge(argBoard Board) (*Board, error) {
 		b.MidPrice = argBoard.MidPrice
 	}
 
+	for _, bid := range argBoard.Bids {
+		if bid.Size == 0 {
+			for i, boardBid := range b.Bids {
+				if bid.Price == boardBid.Price && bid.Size == boardBid.Size {
+					b.Bids = append(b.Bids[:i], b.Bids[i+1:]...)
+				}
+			}
+		} else {
+			b.Bids = append(b.Bids, bid)
+		}
+	}
+
+	for _, ask := range argBoard.Asks {
+		if ask.Size == 0 {
+			for i, boardAsk := range b.Asks {
+				if ask.Price == boardAsk.Price && ask.Size == boardAsk.Size {
+					b.Asks = append(b.Asks[:i], b.Asks[i+1:]...)
+				}
+			}
+		} else {
+			b.Asks = append(b.Asks, ask)
+		}
+	}
 	return b, nil
 }
