29daa7e81bd897a310eb7c133bcb7765896f4831
uchiyama <r.uchiyama@sint.co.jp>
2019-09-04 19:14:46 +0900

    update candleCollection add Merge Candle

--- models/candleCollection.go
+++ models/candleCollection.go
@@ -1,5 +1,17 @@
 package models
 
+import (
+	"time"
+	"fmt"
+)
+
 type CandleCollection struct {
+	ProductCode string
+	Duration time.Duration
 	Candles map[string]Candle
+}
+
+func (c *CandleCollection) MergeCandle(candle Candle) {
+	key := fmt.Sprintf("%s_%s",c.ProductCode, c.Duration)
+	c.Candles[key] = candle
 }
