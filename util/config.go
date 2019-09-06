6a019179b438b78394501fb3c363fe2b7e7d7f22
uchiyama <r.uchiyama@sint.co.jp>
2019-09-06 12:02:35 +0900

    update source gofmt

--- util/config.go
+++ util/config.go
@@ -2,23 +2,27 @@ package util
 
 import (
 	"os"
+	"path/filepath"
 	"runtime"
-	"path/filepath" 
 )
 
-func CreateConfigDirectoryIfNotExists(appName string) (string,error) {
+func CreateConfigDirectoryIfNotExists(appName string) (string, error) {
 	var configDir string
 	home := os.Getenv("HOME")
 	if home == "" && runtime.GOOS == "windows" {
-		configDir = filepath.Join(os.Getenv("APPDATA") , appName)
-		}else {
-		configDir = filepath.Join(home, "." + appName)
-		}
-		if _ ,err := os.Stat(configDir); os.IsNotExist(err) {
-	if err := os.Mkdir(configDir , 0774); err != nil {
-		return "", err
+		configDir = filepath.Join(os.Getenv("APPDATA"), appName)
+	} else {
+		configDir = filepath.Join(home, "."+appName)
 	}
-	return configDir ,nil 
+	if _, err := os.Stat(configDir); os.IsNotExist(err) {
+		if err := os.Mkdir(configDir, 0774); err != nil {
+			return "", err
 		}
-		return configDir ,nil
+		return configDir, nil
+	}
+	return configDir, nil
+}
+
+func SaveConfig(appName string, data interface{}) {
+
 }
