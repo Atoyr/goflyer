8bfa9b53d7237639cf5bd06013989b1a2e118128
atoyr <lessironglance@gmail.com>
2019-09-18 10:39:21 +0900

    add NewMacd

--- models/movingAverageConvergenceDivergence.go
+++ models/movingAverageConvergenceDivergence.go
@@ -1,10 +1,40 @@
 package models
 
 type MovingAverageConvergenceDivergence struct {
-	FastPeriod int
-	SlowPeriod int
+	FastPeriod   int
+	SlowPeriod   int
 	SignalPeriod int
-	Macd []float64
-	MacdSignal []float64
-	MacdHist []float64
+	Macd         []float64
+	MacdSignal   []float64
+	MacdHist     []float64
+	fastEma      Ema
+	slowEma      Ema
+	signalEma    Ema
+}
+
+func NewMovingAverageConvergenceDivergence(inReal []float64, fastPeriod, slowPeriod, signalPeriod int) MovingAverageConvergenceDivergence {
+	var movingAverageConvergenceDivergence MovingAverageConvergenceDivergence
+	fastEma := NewEma(inReal, fastPeriod)
+	slowEma := NewEma(inReal, slowPeriod)
+	macd := make([]float64, len(fastEma.Values))
+	for i := range macd {
+		macd[i] = fastEma.Values[i] - slowEma.Values[i]
+	}
+	signalEma := NewEma(macd, signalPeriod)
+	hist := make([]float64, len(signalEma.Values))
+	for i := range hist {
+		hist[i] = macd[i] - signalEma.Values[i]
+	}
+
+	movingAverageConvergenceDivergence.FastPeriod = fastPeriod
+	movingAverageConvergenceDivergence.SlowPeriod = slowPeriod
+	movingAverageConvergenceDivergence.SignalPeriod = signalPeriod
+	movingAverageConvergenceDivergence.Macd = macd
+	movingAverageConvergenceDivergence.MacdSignal = signalEma.Values
+	movingAverageConvergenceDivergence.MacdHist = hist
+	movingAverageConvergenceDivergence.fastEma = fastEma
+	movingAverageConvergenceDivergence.slowEma = slowEma
+	movingAverageConvergenceDivergence.signalEma = signalEma
+
+	return movingAverageConvergenceDivergence
 }
