ce3a639bd92eaa8eae444d30c25aaf2ba8769126
atoyr <lessironglance@gmail.com>
2019-09-18 11:04:31 +0900

    add update func with Macd

--- models/movingAverageConvergenceDivergence.go
+++ models/movingAverageConvergenceDivergence.go
@@ -14,17 +14,11 @@ type MovingAverageConvergenceDivergence struct {
 
 func NewMovingAverageConvergenceDivergence(inReal []float64, fastPeriod, slowPeriod, signalPeriod int) MovingAverageConvergenceDivergence {
 	var movingAverageConvergenceDivergence MovingAverageConvergenceDivergence
-	fastEma := NewEma(inReal, fastPeriod)
-	slowEma := NewEma(inReal, slowPeriod)
-	macd := make([]float64, len(fastEma.Values))
-	for i := range macd {
-		macd[i] = fastEma.Values[i] - slowEma.Values[i]
-	}
-	signalEma := NewEma(macd, signalPeriod)
-	hist := make([]float64, len(signalEma.Values))
-	for i := range hist {
-		hist[i] = macd[i] - signalEma.Values[i]
-	}
+	fastEma := NewEma([]float64{}, fastPeriod)
+	slowEma := NewEma([]float64{}, slowPeriod)
+	signalEma := NewEma([]float64{}, signalPeriod)
+	macd := make([]float64, 0)
+	hist := make([]float64, 0)
 
 	movingAverageConvergenceDivergence.FastPeriod = fastPeriod
 	movingAverageConvergenceDivergence.SlowPeriod = slowPeriod
@@ -35,6 +29,26 @@ func NewMovingAverageConvergenceDivergence(inReal []float64, fastPeriod, slowPer
 	movingAverageConvergenceDivergence.fastEma = fastEma
 	movingAverageConvergenceDivergence.slowEma = slowEma
 	movingAverageConvergenceDivergence.signalEma = signalEma
+	movingAverageConvergenceDivergence.Update(inReal)
 
 	return movingAverageConvergenceDivergence
+}
+
+func (m *MovingAverageConvergenceDivergence) Update(inReal []float64) {
+	if difflength := len(m.Macd) - len(inReal); difflength > 0 {
+		m.fastEma.Update(inReal)
+		m.slowEma.Update(inReal)
+		macd := make([]float64, difflength)
+		for i := range macd {
+			macd[i] = m.fastEma.Values[difflength+i] - m.slowEma.Values[difflength+i]
+		}
+		m.Macd = append(m.Macd, macd...)
+		m.signalEma.Update(m.Macd)
+		m.MacdSignal = m.signalEma.Values
+		hist := make([]float64, difflength)
+		for i := range hist {
+			hist[i] = m.Macd[difflength+1] - m.MacdSignal[difflength+i]
+		}
+		m.MacdHist = append(m.MacdHist, hist...)
+	}
 }
