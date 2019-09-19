59ab7610adcca3e26fd791772c0646fbcec148b4
atoyr <lessironglance@gmail.com>
2019-09-20 00:44:39 +0900

    add new rsi

--- models/relativeStrengthIndex.go
+++ models/relativeStrengthIndex.go
@@ -3,4 +3,32 @@ package models
 type RelativeStrengthIndex struct {
 	Period int
 	Values []float64
+	diff   []float64
+}
+
+func NewRelativeStrengthIndex(inReal []float64, inTimePeriod int) RelativeStrengthIndex {
+	var rsi RelativeStrengthIndex
+	rsi.Period = inTimePeriod
+
+	rsi.Values = make([]float64, len(inReal))
+	rsi.diff = make([]float64, len(inReal))
+
+	if len(inReal) > 0 {
+		beforeValue := inReal[0]
+		for i := 1; i < len(inReal); i++ {
+			rsi.diff[i] = inReal[i] - beforeValue
+			beforeValue = inReal[i]
+		}
+		// TODO
+		increase := 0.0
+		decrease := 0.0
+		for i := inTimePeriod - 1; i < len(inReal); i++ {
+			if rsi.diff[i] > 0 {
+				increase += rsi.diff[i]
+			} else {
+				decrease -= rsi.diff[i]
+			}
+		}
+	}
+	return rsi
 }
