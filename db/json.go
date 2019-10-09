a61e39f74c54a666c6ee56a1fa0ce2983becbaaa
atoyr <lessironglance@gmail.com>
2019-10-09 23:49:35 +0900

    update add executor and DBinterface jsonDB

--- db/json.go
+++ db/json.go
@@ -1,13 +1,53 @@
 package db
 
 import (
+	"io/ioutil"
+	"os"
+
+	"github.com/atoyr/goflyer/models"
 	"github.com/atoyr/goflyer/util"
 )
 
-func ExportJsonForTickers(db DB ,path string) error {
+type JsonDB struct {
+	tickers []models.Ticker
+}
+
+func GetJsonDB() (JsonDB, error) {
+	var jsonDB JsonDB
+
+	return jsonDB, nil
+}
+
+func (j *JsonDB) Init() error {
+	return nil
+}
+
+func (j *JsonDB) UpdateTicker(models.Ticker) error {
+	return nil
+}
+
+func (j *JsonDB) GetTickerAll() ([]models.Ticker, error) {
+	jsonFile, err := os.Open("./testdata/tickers.json")
+	if err != nil {
+		return nil, err
+	}
+	defer jsonFile.Close()
+	raw, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+	tickers, err := models.JsonUnmarshalTickers(raw)
+	if err != nil {
+		return nil, err
+	}
+	j.tickers = tickers
+	return j.tickers, nil
+}
+
+func ExportJsonForTickers(db DB, path string) error {
 	tickers, err := db.GetTickerAll()
 	if err != nil {
 		return err
 	}
-	return util.JsonMarshalIndent(tickers,path)
+	return util.JsonMarshalIndent(tickers, path)
 }
--- executor/executor.go
+++ executor/executor.go
@@ -1,8 +1,11 @@
 package executor
 
 import (
+	"context"
+	"fmt"
 	"sync"
 
+	"github.com/atoyr/goflyer/client"
 	"github.com/atoyr/goflyer/db"
 	"github.com/atoyr/goflyer/models"
 )
@@ -21,8 +24,19 @@ func GetExecutor(db db.DB) *executor {
 	once.Do(func() {
 		e := new(executor)
 		e.dataFrames = make(map[string]models.DataFrame, 0)
-		e.db = db
 		exe = e
 	})
+	exe.db = db
 	return exe
+}
+
+func RunClient() {
+	client := client.New("", "")
+	var tickerChannl = make(chan models.Ticker)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	client.GetRealtimeTicker(ctx, tickerChannl, "BTC_JPY")
+	for ticker := range tickerChannl {
+		fmt.Println(ticker)
+	}
 }
