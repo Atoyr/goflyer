c3fdfa2908af5613cccefc6404460a18c89f8817
atoyr <lessironglance@gmail.com>
2019-10-11 12:56:18 +0900

    update add GetTicker

--- db/json.go
+++ db/json.go
@@ -22,6 +22,19 @@ func (j *JsonDB) UpdateTicker(models.Ticker) error {
 	return nil
 }
 
+func (j *JsonDB) GetTicker(tickID float64) (models.Ticker, error) {
+	tickers, err := j.GetTickerAll()
+	if err != nil {
+		return models.Ticker{}, err
+	}
+	for i := range tickers {
+		if tickers[i].TickID == tickID {
+			return tickers[i], nil
+		}
+	}
+	return models.Ticker{}, nil
+}
+
 func (j *JsonDB) GetTickerAll() ([]models.Ticker, error) {
 	jsonFile, err := os.Open("./testdata/tickers.json")
 	if err != nil {
