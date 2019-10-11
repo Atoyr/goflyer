d5aef3d5f736e235b6ca06d10e5d370d743b3fa6
atoyr <lessironglance@gmail.com>
2019-10-11 12:57:13 +0900

    update remove handler wrap

--- backend/handle.go
+++ backend/handle.go
@@ -1,18 +1,12 @@
 package backend
 
 import (
-	"github.com/atoyr/goflyer/models"
 	"github.com/labstack/echo"
 )
 
-func GetEcho(ccs models.DataFrames) *echo.Echo {
+func GetEcho() *echo.Echo {
 	e := echo.New()
 	e.Static("/", "public/")
-	e.Use(func(h echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			return h(c)
-		}
-	})
 
 	return e
 }
