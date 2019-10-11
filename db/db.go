735cb3410f5e60311b670b8019ace49cc316cae7
atoyr <lessironglance@gmail.com>
2019-10-11 12:56:42 +0900

    add func GetTicker

--- db/db.go
+++ db/db.go
@@ -6,6 +6,7 @@ import (
 
 type DB interface {
 	UpdateTicker(ticker models.Ticker) error
+	GetTicker(tickerID float64) (models.Ticker, error)
 	GetTickerAll() ([]models.Ticker, error)
 	//	MergeCandle(candle models.Candle)
 	//	GetCandles(productCode string) ([]models.Candle, error)
