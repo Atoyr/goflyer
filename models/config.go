46ecec45b1d4f2c5a0a234ab8f940dc2c13dffcb
atoyr <lessironglance@gmail.com>
2019-10-16 08:48:31 +0900

    update add config model (will move file)

--- models/config.go
+++ models/config.go
@@ -1,20 +1,37 @@
 package models
 
+import (
+	"path/filepath"
+
+	"github.com/atoyr/goflyer/db"
+	"github.com/atoyr/goflyer/util"
+)
+
 type config struct {
 	apppath     string
 	apikey      string
 	timeoutmsec int64
 }
 
+// TODO move config
 // GetConfig is Getting config
 // if path is empty this use default config path
-func GetConfig(path string) config {
+func GetConfig(path string) (config, error) {
 	var c config
 	apppath := path
 	if apppath == "" {
+
+		dirPath, err := util.CreateConfigDirectoryIfNotExists("goflyer")
+		if err != nil {
+			return c, err
+		}
+		dbfile := filepath.Join(dirPath, "goflyer.db")
+		boltdb, err := db.GetBolt(dbfile)
+		if err != nil {
+			return c, err
+		}
 		apppath = ""
 	}
-
 	c.apppath = apppath
 	return c
 }
