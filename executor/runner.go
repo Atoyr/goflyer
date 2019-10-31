47eef03dbccadbd57279e2344661148c8cee1dc0
Atoyr <lessironglance@gmail.com>
2019-10-31 19:24:26 +0900

    add executor runner

---
+++ executor/runner.go
@@ -0,0 +1,16 @@
+package executor
+
+import (
+  "context"
+	"github.com/atoyr/goflyer/models"
+)
+
+func RunAsync(ctx context.Context) {
+	executor := GetExecutor()
+	callback := make([]func(beforeticker,ticker models.Ticker),1)
+	callback = append(callback , func(beforeticker,ticker models.Ticker) {
+		executor.AddValue(ticker.DateTime(), ticker.TickID,ticker.GetMidPrice(),ticker.Volume)
+		executor.SaveCandles()
+	})
+	executor.FetchTickerAsync(ctx, callback )
+}
