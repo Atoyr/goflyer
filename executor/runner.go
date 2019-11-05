54dc3540414f1eddc34d4f1c4f8282b586d4992d
Atoyr <lessironglance@gmail.com>
2019-11-05 13:15:07 +0900

    fix add candle value

--- executor/runner.go
+++ executor/runner.go
@@ -9,7 +9,7 @@ func RunAsync(ctx context.Context) {
 	executor := GetExecutor()
 	callback := make([]func(beforeticker,ticker models.Ticker),0)
 	callback = append(callback , func(beforeticker,ticker models.Ticker) {
-		executor.AddValue(ticker.DateTime(), ticker.TickID,ticker.GetMidPrice(),ticker.Volume)
+		executor.AddValue(ticker.DateTime(), ticker.GetMidPrice(),ticker.Volume)
 		executor.SaveCandles()
 	})
 	executor.FetchTickerAsync(ctx, callback )
