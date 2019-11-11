d1c70306976c10a951876c2b773d359eff2bd63c
atoyr <lessironglance@gmail.com>
2019-11-12 06:50:44 +0900

    add print candlestick when execute runner

--- executor/runner.go
+++ executor/runner.go
@@ -2,6 +2,7 @@ package executor
 
 import (
   "context"
+	"fmt"
 
 	"github.com/atoyr/goflyer/models"
 )
@@ -11,6 +12,17 @@ func RunAsync(ctx context.Context) {
 	callback = append(callback , func(beforeticker,ticker models.Ticker) {
 		Add(ticker.DateTime(), ticker.Ltp,ticker.Volume)
 		SaveDataFrame()
+		exe := getExecutor()
+		df := exe.dataFrames[1]
+		last := len(df.Datetimes) -1
+		lastDatetime := ticker.DateTime()
+		x := beforeticker.DateTime().Truncate(models.GetDuration(models.Duration_3m))
+		y := ticker.DateTime().Truncate(models.GetDuration(models.Duration_3m))
+		if !x.Equal(y){
+			lastDatetime = beforeticker.DateTime()
+			fmt.Println()
+		}
+		fmt.Printf("\r%s open : %f high : %f low : %f close : %f %s",df.Datetimes[last],df.Opens[last],df.Highs[last],df.Lows[last],df.Closes[last],lastDatetime)
 	})
 	FetchTickerAsync(ctx, callback )
 } 
