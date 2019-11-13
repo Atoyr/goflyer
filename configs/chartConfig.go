ab02967302158cb7599c6208c649e76d68ab0371
atoyr <lessironglance@gmail.com>
2019-11-13 23:56:50 +0900

    add chart config type add json tag

--- configs/chartConfig.go
+++ configs/chartConfig.go
@@ -1,22 +1,26 @@
 package configs
 
 type ChartConfig struct {
-	DataFrameConfigs []DataFrameConfig
+	DataFrameConfigs []DataFrameConfig `json:"data_frame_configs"`
 }
 
 type DataFrameConfig struct {
-	Duration string
-	Smas []MovingAveragePreference
-	Emas []MovingAveragePreference
-	Macd []MACDPreference
+	Duration string                    `json:"duration"`
+	Smas     []MovingAveragePreference `json:"smas"`
+	Emas     []MovingAveragePreference `json:"emas"`
+	Macd     []MACDPreference          `json:"macd"`
 }
 
-type MovingAveragePreference struct{ 
-	Period int 
+type MovingAveragePreference struct {
+	Period int `json:"period"`
 }
 
 type MACDPreference struct {
 	FastPeriod   int       `json:"fast_period"`
 	SlowPeriod   int       `json:"slow_period"`
 	SignalPeriod int       `json:"signal_period"` 
+}
+
+func (CC *ChartConfig) Save(){
+
 }
