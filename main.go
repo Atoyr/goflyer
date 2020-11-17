a9a402a580b4734f31774f58dd079f882bcc3037
atoyr <lessironglance@gmail.com>
2020-11-17 19:52:19 +0900

    update main

--- main.go
+++ main.go
@@ -36,34 +36,27 @@ func main() {
       ch := make(chan []bitflyer.Execution)
       go cli.GetRealtimeExecutions(ctx,ch,"BTC_JPY")
       fmt.Println("execute")
-      candle := new(models.Candle)
-      candle.Time = time.Now().Truncate(duration)
       ticker := time.NewTicker(30 * time.Second)
       df := models.NewDataFrame("BTC_JPY", 5 * time.Minute)
+      t := time.Now().Truncate(1 * time.Minute)
       for {
         select {
         case <-ctx.Done():
           return nil
         case <-ticker.C:
           df.ApplyExecution()
-          fmt.Println()
-          fmt.Println("dataframe")
-          for i := range df.Datetimes {
-            fmt.Printf("  time : %s , Open : %7.0f , High : %7.0f , Low : %7.0f , Close : %7.0f , Volume : %f",df.Datetimes[i], df.Opens[i], df.Highs[i], df.Lows[i], df.Closes[i], df.Volumes[i])
+          i := len(df.Datetimes) - 1
+          if df.Datetimes[i].Equal(t) {
+            fmt.Printf("\r")
+          }else {
             fmt.Println()
+            t = df.Datetimes[i]
           }
+          fmt.Printf("Time : %s , Open : %7.0f , High : %7.0f , Low : %7.0f , Close : %7.0f , Volume : %f",df.Datetimes[i], df.Opens[i], df.Highs[i], df.Lows[i], df.Closes[i], df.Volumes[i])
         case param := <-ch:
           for i := range  param {
-            t := candle.Time
-            mergeCandle(candle, param[i].DateTime(), param[i].Price)
             e := models.Execution{ Side: param[i].Side, Price : param[i].Price, Size : param[i].Size, Time : param[i].DateTime()}
             df.AddExecution(e)
-            if t.Equal(candle.Time) {
-              fmt.Printf("\r%s",candle.String())
-            }else {
-              fmt.Printf("\r%s",candle.String())
-              fmt.Println()
-            }
           }
         }
       }
@@ -74,23 +67,4 @@ func main() {
   if err != nil {
     log.Fatal(err)
   }
-}
-
-func mergeCandle(c *models.Candle, execDate time.Time, price float64) *models.Candle {
-  dt := execDate.Truncate(duration)
-  if c.Time.Equal(dt) {
-			c.Close = price
-			if c.High < price {
-				c.High = price
-			} else if c.Low > price {
-				c.Low = price
-			}
-  }else {
-    c.Time = dt
-    c.Open = price
-    c.High = price
-    c.Low = price
-    c.Close = price
-  }
-  return c
 }
