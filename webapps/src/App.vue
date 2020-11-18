dce514f844fe87028dfaeab8c6602b1804090819
atoyr <lessironglance@gmail.com>
2020-11-18 19:44:05 +0900

    add webapps

---
+++ webapps/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="contents">
+      <GChart
+        type="ColumnChart"
+        :data="chartData"
+        :options="chartOptions"
+      />
+    </div>
+</template>
+
+<script>
+import { GChart } from 'vue-google-charts';
+import fetch from 'node-fetch';
+
+export default {
+  components: {
+    GChart
+  },
+  mounted() {
+    this.intervalId = setInterval(function () {
+      fetch('http://localhost:8080/candles',{mode: "no-cors",})
+        .then(res => {
+          console.log(res)
+          return res.json()
+        })
+        .then(data => {
+          this.chartData = []
+          for (const c of data) {
+            let temp = []
+            temp.push(c.time)
+            temp.push(c.low)
+            temp.push(c.open)
+            temp.push(c.close)
+            temp.push(c.high)
+            this.chartData.push(temp)
+          }
+        })}, 1000)
+  },
+  beforeDestroy() {
+    clearInterval(this.intervalId)
+  },
+  data() {
+    return {
+      chartData: [ ],
+      chartOptions: {
+        title: 'Company Performance',
+        subtitle: 'Sales'
+      },
+      intervalId : undefined
+    };
+  }
+};
+</script>
